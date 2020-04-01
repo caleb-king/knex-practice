0785b43fdb3ca1164ff0bc8363b37e97995c6e9d
Caleb King <caleb.king536@gmail.com>
2020-04-01 16:33:13 -0600

    complete tutorial for blogful service including testing the service

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,24 @@
+insert into blogful_articles
+  (title, content, date_published) 
+  values
+    ('Beddoes', 'Beddoes is a surname of Welsh origin', now() - '29 days'::INTERVAL),
+    ('Brook Chine', 'Brook Chine is a geological feature on the south west coast of the Isle of Wight, England.', now() - '65 days'::INTERVAL),
+    ('Calocosmus fulvicollis', 'Calocosmus fulvicollis is a species of beetle in the family Cerambycidae.', now() - '57 days'::INTERVAL),
+    ('Ottaviani', 'Ottaviani is an Italian surname.', now() - '56 days'::INTERVAL),
+    ('Aliabad-e Jombozeh', 'is a village in Qombovan Rural District, in the Central District of Dehaqan County, Isfahan Province, Iran. At the 2006 census, its population was 1,305, in 355 families.', now() - '43 days'::INTERVAL),
+    ('Richard von Schubert-Soldern', 'Richard Ritter von Schubert-Soldern (14 December 1852, Prague – 19 October 1924[1]) was a German philosopher.', now() - '46 days'::INTERVAL),
+    ('Get Fresh', '"Get Fresh" is the third single from Kid Sister''s debut studio album Ultraviolet.', now() - '49 days'::INTERVAL),
+    ('1976 in South African television', 'This is a list of South African television-related events in 1976.', now() - '46 days'::INTERVAL),
+    ('Athletics at the 2019 African Games – Women''s 400 metres', 'The women''s 400 metres event at the 2019 African Games was held on 26, 27 and 28 August in Rabat', now() - '35 days'::INTERVAL),
+    ('William Sherman Jennings House', 'The William Sherman Jennings House (also known as the May Mann Jennings House or the Jennings-Rogers House) is a historic residence in Brooksville, Florida, United States.', now() - '39 days'::INTERVAL),
+    ('2008 OFC Champions League Final', 'The final of the 2007–08 OFC Champions League was played between Waitakere United of New Zealand and Kossa FC of the Solomon Islands.', now() - '33 days'::INTERVAL),
+    ('Benny Benassi discography', 'This is a discography of the Italian DJ and producer Benny Benassi.', now() - '35 days'::INTERVAL),
+    ('Slawko Klymkiw', 'Slawko Klymkiw is a Canadian philanthropist who has been the long-running CEO of the Canadian Film Centre.', now() - '36 days'::INTERVAL),
+    ('King of the Hill', 'King of the Hill is an American animated sitcom created by Mike Judge and Greg Daniels for the Fox Broadcasting Company that ran from January 12, 1997 to May 6, 2010.', now() - '39 days'::INTERVAL),
+    ('Canton of Béziers-2', 'The canton of Béziers-2 is an administrative division of the Hérault department, southern France.', now() - '23 days'::INTERVAL),
+    ('List of plastic surgery flaps', 'Below is a list of common and eponymous plastic surgery flaps with their classification and common usage.', now() - '25 days'::INTERVAL),
+    ('John Stevenson (director)', 'John Stevenson (born 1958) is a British animation filmmaker and puppeteer best known for directing the 2008 animated feature film Kung Fu Panda with Mark Osborne.', now() - '13 days'::INTERVAL),
+    ('Marek Cieślak', 'Marek Kazimierz Cieślak (born 28 June 1950 in Milanówek, Poland)[1] is a former Polish speedway rider and a current speedway coach.', now() - '19 days'::INTERVAL),
+    ('U.S. Navy Diving Manual', 'The U.S. Navy Diving Manual is a book used by the US Navy for diver training and diving operations.', now() - '7 days'::INTERVAL),
+    ('Nazlı Savranbaş', 'Nazlı Savranbaşı (born 9 October 2003) is a Turkish female artistic gymnast.', now() - '3 days'::INTERVAL)
+;
